b834a50766db5545d52c51ab0935a192e53d84ce
ruizhao <zr1998@mail.ustc.edu.cn>
2018-04-05 20:30:20 +0800

    test the ^ operation

--- PB16120853/unit_tsts.cpp
+++ PB16120853/unit_tsts.cpp
@@ -1,8 +1,6 @@
 #include "number.hpp"
 #include<iostream>
-
-
-int number_test() {
+void number_test() {
 	fraction a(1, 0), b(0, 1), c(2, 3), d(5, 6), e(8, 4), g(18, 9);
 	fraction x;
 	x = a + b;
@@ -21,5 +19,19 @@ int number_test() {
 	x = c / d;
 	x = c + d;
 	x = c - d;
+	x = a ^ b;
+	x = a ^ c;
+	x = a ^ d;
+	x = a ^ e;
+	x = a ^ g;
+	x = b ^ c;
+	x = b ^ a;
+	x = b ^ d;
+	x = b ^ e;
+	x = b ^ g;
+	x = e * g;
+	x = e - g;
+	x = e + g;
+	x = e / g;
 
 }
